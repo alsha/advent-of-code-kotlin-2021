029b08c33022ba99a8f4023a12150d56a9a54096
alsha <alexey.shakov@gmail.com>
2021-12-05 13:05:06 +0100

    refactor

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,8 +1,49 @@
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.system.measureTimeMillis
+
 fun main() {
 
-    fun part1(input: List<String>): Int {
+    data class Point (val x: Int, val y: Int) {}
 
-        return -1
+    data class Line (val start: Point, val end: Point) {}
+
+    fun isLineVertical(line: List<List<Int>>) =
+        line[0][0] == line[1][0]
+
+    fun isLineHorizontal(line: List<List<Int>>) =
+        line[0][1] == line[1][1]
+
+    fun part1(input: List<String>): Int {
+        val allLines = input
+            .map { line ->
+                line.trim().split("->")
+                    .map { xy ->
+                        xy.trim().split(",").map { it.toInt() }
+                    }
+            }
+
+        val pointsOfAllHorizontalLines = allLines
+            .filter { line -> isLineHorizontal(line) }
+            .map { line ->
+                (min(line[0][0], line[1][0])..max(line[0][0], line[1][0])).map { listOf(it, line[0][1]) }
+            }.flatten()
+
+        val pointsOfAllVerticalLines = allLines
+            .filter { line -> isLineVertical(line) }
+            .map { line ->
+                (min(line[0][1], line[1][1])..max(line[0][1], line[1][1])).map { listOf(line[0][0], it) }
+            }.flatten()
+
+
+        val allPoints =  pointsOfAllHorizontalLines.plus(pointsOfAllVerticalLines)
+
+        val result = allPoints
+            .groupingBy { it }
+            .eachCount()
+            .filter { it.value > 1 }.size
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -13,11 +54,14 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    part1(testInput)
-    check(part1(testInput) == 4512)
-    check(part2(testInput) == 1924)
+    //part1(testInput)
+    check(part1(testInput) == 5)
+    //check(part2(testInput) == 1924)
 
     val input = readInput("Day05")
-    println(part1(input))
-    println(part2(input))
+    val timeInMillis = measureTimeMillis {
+        println(part1(input))
+    }
+    println("(The operation took $timeInMillis ms)")
+    //println(part2(input))
 }
