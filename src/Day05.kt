9f2bb69c7ffed486769572940c6cc6d30ba73d81
alsha <alexey.shakov@gmail.com>
2021-12-05 14:02:08 +0100

    Day5

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,42 +1,56 @@
-import kotlin.math.max
-import kotlin.math.min
+import kotlin.math.abs
 import kotlin.system.measureTimeMillis
 
 fun main() {
 
-    data class Point (val x: Int, val y: Int) {}
+    data class Point(val x: Int, val y: Int) {}
 
-    data class Line (val start: Point, val end: Point) {}
+    data class Line(val start: Point, val end: Point) {}
 
-    fun isLineVertical(line: List<List<Int>>) =
-        line[0][0] == line[1][0]
+    fun isLineVertical(line: Line) =
+        line.start.x == line.end.x
 
-    fun isLineHorizontal(line: List<List<Int>>) =
-        line[0][1] == line[1][1]
+    fun isLineHorizontal(line: Line) =
+        line.start.y == line.end.y
 
-    fun part1(input: List<String>): Int {
+    fun isLineDiagonal(line: Line) =
+        abs(line.start.x - line.end.x) == abs(line.start.y - line.end.y)
+
+    fun pointsOfDiagonalLine(line: Line): List<Point> =
+        (line.start.x through line.end.x).zip(line.start.y through line.end.y).map { Point(it.first, it.second) }
+
+    fun part1(input: List<String>, diagonal: Boolean = false): Int {
         val allLines = input
             .map { line ->
                 line.trim().split("->")
                     .map { xy ->
                         xy.trim().split(",").map { it.toInt() }
-                    }
-            }
+                    }.map { Point(it[0], it[1]) }
+            }.map { Line(it[0], it[1]) }
 
         val pointsOfAllHorizontalLines = allLines
             .filter { line -> isLineHorizontal(line) }
             .map { line ->
-                (min(line[0][0], line[1][0])..max(line[0][0], line[1][0])).map { listOf(it, line[0][1]) }
+                (line.start.x through line.end.x).map { x -> Point(x, line.start.y) }
             }.flatten()
 
         val pointsOfAllVerticalLines = allLines
             .filter { line -> isLineVertical(line) }
             .map { line ->
-                (min(line[0][1], line[1][1])..max(line[0][1], line[1][1])).map { listOf(line[0][0], it) }
+                (line.start.y through line.end.y).map { y -> Point(line.start.x, y) }
             }.flatten()
 
 
-        val allPoints =  pointsOfAllHorizontalLines.plus(pointsOfAllVerticalLines)
+        var allPoints = pointsOfAllHorizontalLines.plus(pointsOfAllVerticalLines)
+
+        if (diagonal) {
+            val pointsOfAllDiagonalLines = allLines
+                .filter { line -> isLineDiagonal(line) }
+                .map { line -> pointsOfDiagonalLine(line) }
+                .flatten()
+
+            allPoints = allPoints.plus(pointsOfAllDiagonalLines)
+        }
 
         val result = allPoints
             .groupingBy { it }
@@ -46,22 +60,20 @@ fun main() {
         return result
     }
 
-    fun part2(input: List<String>): Int {
-
-        return -1
-    }
-
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    //part1(testInput)
     check(part1(testInput) == 5)
-    //check(part2(testInput) == 1924)
+    check(part1(testInput, true) == 12)
 
     val input = readInput("Day05")
     val timeInMillis = measureTimeMillis {
         println(part1(input))
+        println(part1(input, true))
     }
     println("(The operation took $timeInMillis ms)")
-    //println(part2(input))
+}
+
+private infix fun Int.through(i: Int): Iterable<Int> {
+    return if (this < i) this..i else this downTo i
 }
