86ba2eb3322172941d912a269a815fbd65c59cb0
alsha <alexey.shakov@gmail.com>
2021-12-07 11:51:18 +0100

    Day7 V.1

---
+++ src/Day07.kt
@@ -0,0 +1,26 @@
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
+
+fun main() {
+
+
+    fun part1(input: List<String>): Int {
+        return -1
+    }
+
+    fun part2(input: List<String>): Int {
+        return -1
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == -1)
+    check(part2(testInput) == -1)
+
+    val input = readInput("Day07")
+    val timeInMillis = measureTimeMillis {
+        println(part1(input))
+        println(part2(input))
+    }
+    println("(The operation took $timeInMillis ms)")
+}
